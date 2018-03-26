dd711b9bf12abf92bddc56293127947525dc0297
huisukgyeong <huisuk.gyeong@connect.qut.edu.au>
2018-03-27 07:09:17 +1000

    UI FIX
    Button fix

---
+++ Old generation/SuperHumans/SuperHumans/SuperHumans/Model/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperHumans.Model
+{
+    public class Question
+    {
+        public string ObjectId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Owner { get; set; }
+        public string TimeAgo { get; set; }
+    }
+}
---
+++ Old generation/SuperHumans/SuperHumans/SuperHumans/View/MainNavi.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SuperHumans.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainNavi : ContentPage
+	{
+		public MainNavi ()
+		{
+			InitializeComponent ();
+            MainNaviListView.ItemsSource = new List<ListViewTemplate>
+            {
+                new ListViewTemplate{Name="QA",Description="Main Navi 1",OrderNum=1},
+                new ListViewTemplate { Name = "SuperHuman", Description = "Main Navi 2", OrderNum = 2 },
+            };
+        }
+
+        async void OnTapped(object sender, ItemTappedEventArgs e)
+        {
+            var Selected = e.Item as ListViewTemplate;
+            //DisplayAlert("Item Tapped", Selected.Name, "Okay");
+            switch (Selected.OrderNum)
+            {
+                case 1:
+                    await Navigation.PushAsync(new QA_Page());
+                    break;
+                case 2:
+                    await Navigation.PushAsync(new Superhuman());
+                    break;
+            }
+            //Remove selected colour when touched
+            ((ListView)sender).SelectedItem = null;
+
+        }
+
+        public class ListViewTemplate
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public int OrderNum { get; set; }
+        }
+    }
+}
